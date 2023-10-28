16a352ceb76fb4e96acfc419618afe3b2c596add
dani ferdinan <daniferdinandall@gmail.com>
2023-10-28 18:08:40 +0700

    fix login

--- module/handler.go
+++ module/handler.go
@@ -3,11 +3,13 @@ package betrens
 import (
 	"encoding/json"
 	"net/http"
+	"os"
 
 	model "github.com/trensentimen/be_trensen/model"
+	"github.com/whatsauth/watoken"
 )
 
-func GCFPostSignup(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+func GCFHandlerSignup(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
 	conn := MongoConnect(MONGOCONNSTRINGENV, dbname)
 	var Response model.Credential
 	Response.Status = false
@@ -24,6 +26,33 @@ func GCFPostSignup(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Re
 	}
 	Response.Status = true
 	Response.Message = "Halo " + dataUser.Name
+	return GCFReturnStruct(Response)
+}
+
+func GCFHandlerSignin(PASETOPRIVATEKEYENV, MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	conn := MongoConnect(MONGOCONNSTRINGENV, dbname)
+	var Response model.Credential
+	Response.Status = false
+	var dataUser model.User
+	err := json.NewDecoder(r.Body).Decode(&dataUser)
+	if err != nil {
+		Response.Message = "error parsing application/json: " + err.Error()
+		return GCFReturnStruct(Response)
+	}
+	user, err := SignIn(conn, collectionname, dataUser)
+	if err != nil {
+		Response.Message = err.Error()
+		return GCFReturnStruct(Response)
+	}
+	Response.Status = true
+	// Response.Message = "Halo " + dataUser.Name
+	tokenstring, err := watoken.Encode(dataUser.Email, os.Getenv(PASETOPRIVATEKEYENV))
+	if err != nil {
+		Response.Message = "Gagal Encode Token : " + err.Error()
+	} else {
+		Response.Message = "Selamat Datang " + user.Email
+		Response.Token = tokenstring
+	}
 	return GCFReturnStruct(Response)
 }
 
