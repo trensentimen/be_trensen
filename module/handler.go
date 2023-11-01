31770fc68c4d6b2f923431e9c0cc62ef61d3908c
dani ferdinan <daniferdinandall@gmail.com>
2023-11-01 16:41:24 +0700

    update getAllTopic

--- module/handler.go
+++ module/handler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 
 	model "github.com/trensentimen/be_trensen/model"
 	"github.com/whatsauth/watoken"
@@ -57,11 +58,28 @@ func GCFHandlerSignin(PASETOPRIVATEKEYENV, MONGOCONNSTRINGENV, dbname, collectio
 	return GCFReturnStruct(Response)
 }
 
-func GCFHandlerGetAllTopic(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+func GCFHandlerGetAllTopic(PASETOPUBLICKEY, MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
 	conn := MongoConnect(MONGOCONNSTRINGENV, dbname)
 	var Response model.TopicResponse
 	Response.Status = false
 	var dataUser model.User
+
+	// get token from header
+	token := r.Header.Get("Authorization")
+	token = strings.TrimPrefix(token, "Bearer ")
+	if token == "" {
+		Response.Message = "error parsing application/json:"
+		return GCFReturnStruct(Response)
+	}
+
+	// decode token
+	_, err1 := watoken.Decode(PASETOPUBLICKEY, token)
+
+	if err1 != nil {
+		Response.Message = "error parsing application/json: " + err1.Error()
+		return GCFReturnStruct(Response)
+	}
+
 	err := json.NewDecoder(r.Body).Decode(&dataUser)
 	if err != nil {
 		Response.Message = "error parsing application/json: " + err.Error()
